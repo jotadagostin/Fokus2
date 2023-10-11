cdf98946a42bba0c49d8a7852a3cd93f87e81b28
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-10-11 09:58:17 +0200

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -1,53 +1,10 @@
-// const taskListContainer = document.querySelector(".app__section-task-list");
+const taskListContainer = document.querySelector(".app__section-task-list");
+const formTask = document.querySelector(".app__form-add-task");
+const toggleFormTaskBtn = document.querySelector(".app__button--add-task");
+const formLabel = document.querySelector(".app__form-label");
+const textarea = document.querySelector(".app__form-textarea");
 
-// let task = [
-//   { description: "task concluded", concluded: true },
-//   { description: "task not-concluded", concluded: false },
-// ];
-
-// const taskIconSvg = `<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
-// fill="none" xmlns="http://www.w3.org/2000/svg">
-// <circle cx="12" cy="12" r="12" 
-//     fill="#FFF" />
-// <path
-//     d "M9 16.1719119.5938 5.57812121 6.9843819 18.9844L3.42188 13.4062L4.82812 12L19 16.17192"
-//     fill="#01080E" 
-// /></svg>`;
-
-// function createTask(task) {
-//   // const to create li, createElemtn to create an element that doesnt exist
-//   const li = document.createElement("li");
-//   li.classList.add("app__section-task-list-item");
-//   //classlist to style the li
-//   const svgIcon = document.createElement("svg");
-//   svgIcon.innerHTML = taskIconSvg;
-//   const paragraph = document.createElement("p");
-//   paragraph.classList.add("app__section-task-list-item-description");
-
-//   paragraph.textContent = task.description;
-//   li.appendChild(svgIcon);
-//   li.appendChild(paragraph);
-
-//   return li;
-// }
-
-// tarefas.forEach((task) => {
-//   const taskItem = createTask(task);
-//   taskListContainer.appendChild(taskItem);
-// });
-
-const taskListContainer = document.querySelector('.app__section-task-list')
-
-let tasks = [
-    {
-        description: 'Task Concluded',
-        concluida: true
-    },
-    {
-        description: 'Task Pending',
-        concluida: false
-    }
-]
+let tasks = [];
 
 const taskIconSvg = `
 <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
@@ -57,26 +14,42 @@ const taskIconSvg = `
         d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
         fill="#01080E" />
 </svg>
-`
+`;
 function createTask(task) {
-    const li = document.createElement('li')
-    li.classList.add('app__section-task-list-item')
+  const li = document.createElement("li");
+  li.classList.add("app__section-task-list-item");
 
-    const svgIcon = document.createElement('svg')
-    svgIcon.innerHTML = taskIconSvg
+  const svgIcon = document.createElement("svg");
+  svgIcon.innerHTML = taskIconSvg;
 
-    const paragraph = document.createElement('p')
-    paragraph.classList.add('app__section-task-list-item-description')
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("app__section-task-list-item-description");
 
-    paragraph.textContent = task.description
+  paragraph.textContent = task.description;
 
-    li.appendChild(svgIcon)
-    li.appendChild(paragraph)
-    
-    return li
+  li.appendChild(svgIcon);
+  li.appendChild(paragraph);
+
+  return li;
 }
 
-tasks.forEach(task => {
-    const taskItem = createTask(task)
-    taskListContainer.appendChild(taskItem)
-})
+tasks.forEach((task) => {
+  const taskItem = createTask(task);
+  taskListContainer.appendChild(taskItem);
+});
+
+toggleFormTaskBtn.addEventListener("click", () => {
+  formLabel.textContent = "Adicionando Tarefa";
+  formTask.classList.toggle("hidden");
+});
+
+formTask.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const task = {
+    description: textarea.value,
+    concluded: false,
+  };
+  tasks.push(task);
+  const taskItem = createTask(task);
+  taskListContainer.appendChild(taskItem);
+});
